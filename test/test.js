637513df660c4f390ae9f8c0d21c50d8ece7dbf9
yohannes sileshi <yohannessileshi1@gmail.com>
2024-05-06 08:56:59 -0500

    done lab6 Q 1 & 2

---
+++ test/test.js
@@ -0,0 +1,28 @@
+function  foo(x)  {
+
+    let m;
+
+console.log(x, y);
+
+if(x > 5){
+
+var  y  =  5; m = x + y;
+
+} else {
+
+let  z  =  10; m = z;
+
+}
+
+x = m;
+
+console.log(x, y);
+
+}
+
+
+var  x  =  10;
+
+foo(3);
+
+console.log(x);
