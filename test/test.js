917370b5328ff087233b8aebfc39774ecd1ad047
yohannes sileshi <yohannessileshi1@gmail.com>
2024-05-13 22:59:12 -0500

    finished lab 10

--- test/test.js
+++ test/test.js
@@ -1,20 +1,23 @@
-x = 1;
-var a = 5;
-var b = 10;
-var c = function (a, b, c) {
-  console.log("x1: " + x);
-  console.log("a2: " + a);
-  var f = function () {
-    b = a;
-    console.log("b3: " + b);
-    b = c;
-    var a = 3;
-  };
-  console.log("testing" + x);
-  f();
-  console.log("b4: " + b);
-  x = 6;
-};
-c(8, 9, 7);
-console.log("b5: " + b);
-console.log("x6: " + x);
+const result = (function(exports, module){
+
+    exports = module.exports;
+
+    exports.firstname = 'John';
+
+    module.exports.lastname = 'Smith';
+
+    exports = {
+
+        getFullName: function(){
+
+            console.log('John Smith')
+
+        }
+
+    }
+
+    return module.exports;
+
+}).apply(null, [null, {exports: {}}]);
+
+console.log(result);
